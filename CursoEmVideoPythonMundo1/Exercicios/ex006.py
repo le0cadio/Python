fc0fe9fafd90dcc45618793a1319215088fb89d6
Leocadio <guilhermeleocadio1301@gmail.com>
2024-08-15 13:03:23 -0300

    fix issue in code

--- CursoEmVideoPythonMundo1/Exercicios/ex006.py
+++ CursoEmVideoPythonMundo1/Exercicios/ex006.py
@@ -1,8 +1,8 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 numero1 = int(input('Digite um número: '))
-dobro = numero1 ** 2
-triplo = numero1 ** 3
+dobro = numero1 * 2
+triplo = numero1 * 3
 raizQuadrada = numero1 ** (1/2)
 
 print(f'O dobro de {numero1} é {dobro} e o triplo de {numero1} é {triplo} e a raiz quadrada de {numero1} é {raizQuadrada}')
