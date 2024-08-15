19cd02cb421821e122465b973cd4c6918431c6f1
Leocadio <guilhermeleocadio1301@gmail.com>
2024-08-15 13:26:05 -0300

    feat: new features

--- CursoEmVideoPythonMundo1/Exercicios/ex008.py
+++ CursoEmVideoPythonMundo1/Exercicios/ex008.py
@@ -1,7 +1,12 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 valor = int(input('Digite um valor: '))
 
-cent = valor * 100
-mili = valor * 1000
+centimetro = valor * 100
+milimetro = valor * 1000
+decametro = valor / 10
+hectometro = valor / 100
+quilometro = valor / 100
 
-print(f'seu valor convertido em centimetros é de: {cent} e convertido em milimetros é de: {mili} ')
+print(f'seu valor convertido em centimetros é de: {centimetro} e convertido em milimetros é de: {milimetro} ')
+print(f' seu valor convertido em decametro é de : {decametro} e convertido em hectometro é de:  {hectometro}')
+print(f'seu valor convertido em quilometro é de : {quilometro}')
