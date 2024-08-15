391a2d043fe251408ede51ea5991f0955f604f48
Leocadio <guilhermeleocadio1301@gmail.com>
2024-08-15 13:13:06 -0300

    fix: nota do aluno exercicio 07

--- CursoEmVideoPythonMundo1/Exercicios/ex007.py
+++ CursoEmVideoPythonMundo1/Exercicios/ex007.py
@@ -1,8 +1,8 @@
 #Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
 
-nota1 = int(input('Digite a sua primeira nota: '))
-nota2 = int(input('Digite a sua segunda nota: '))
+nota1 = float(input('Digite a sua primeira nota: '))
+nota2 = float(input('Digite a sua segunda nota: '))
 
-media = nota1 + nota2 / 2
+media = (nota1 + nota2) / 2
 
 print(f'A média do aluno é: {media}')
