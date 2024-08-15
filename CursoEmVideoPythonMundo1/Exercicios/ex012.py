284c2b1574365c24c72c8e5bdf43b7a00038f677
Leocadio <guilhermeleocadio1301@gmail.com>
2024-08-15 01:32:52 -0300

    ex012

---
+++ CursoEmVideoPythonMundo1/Exercicios/ex012.py
@@ -0,0 +1,7 @@
+#faça um algoritmo que leia o preço do seu produto e mostre seu novo preço, com 5% de desconto
+
+produto = float(input('Digite o valor do seu produto: '))
+
+valor = produto - (produto * 0.05)
+
+print(f'o valor do seu produto com desconto é de {valor}')
