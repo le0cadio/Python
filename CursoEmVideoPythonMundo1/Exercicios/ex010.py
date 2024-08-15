51d9845fbfb173b3b4c17afe91cc80a7493e677d
Leocadio <guilhermeleocadio1301@gmail.com>
2024-08-15 01:24:39 -0300

    ex010

---
+++ CursoEmVideoPythonMundo1/Exercicios/ex010.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem e mostre quantos dólares ela pode comprar
+
+dinheiro = float(input('Digite quanto de dinheiro você tem em R$: '))
+
+dolar = dinheiro / 5.75
+
+print(f'Seu dinheiro pode comprar : {dolar} dolar!')
